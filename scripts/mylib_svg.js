0370804fb8200bb2d086bb5c7b4c8d57fdb2a682
Anshuman Kumar <anshu.viit@gmail.com>
2017-10-16 19:11:44 +0530

    Removed unnecessary code

--- scripts/mylib_svg.js
+++ scripts/mylib_svg.js
@@ -8,37 +8,6 @@ var pdfDoc = null,
     scale = 1.5
 canvas = document.getElementById('content');
 // ctx = canvas.getContext('2d');
-
-function renderPageOld(num) {
-    pageRendering = true;
-    // Using promise to fetch the page
-    pdfDoc.getPage(num).then(function (page) {
-        var viewport = page.getViewport(scale);
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-
-        // Render PDF page into canvas context
-        var renderContext = {
-            canvasContext: ctx,
-            viewport: viewport
-        };
-        var renderTask = page.render(renderContext);
-
-        // Wait for rendering to finish
-        renderTask.promise.then(function () {
-            pageRendering = false;
-            if (pageNumPending !== null) {
-                // New page rendering is pending
-                renderPage(pageNumPending);
-                pageNumPending = null;
-            }
-        });
-    });
-
-    // Update page counters
-    document.getElementById('page_num').textContent = pageNum;
-}
-
 function renderPage(num) {
     pageRendering = true;
     // Using promise to fetch the page
